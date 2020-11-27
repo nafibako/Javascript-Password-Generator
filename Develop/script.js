683a31081b8fac22daa54cb36479647529c5d715
nafi bako <nafibako@yahoo.com>
2020-11-27 16:29:36 -0500

    final commit

---
+++ Develop/script.js
@@ -0,0 +1,80 @@
+
+//Dom elements
+const resultEL = document.getElementById ("result");
+const lengthEl = document.getElementById ("length");
+const uppercaseEl = document.getElementById ("uppercase");
+const lowercaseEl = document.getElementById ("lowercase");
+const numbersEl = document.getElementById ("numbers");
+const symbolsEl = document.getElementById ("symbols");
+const generatetEl = document.getElementById ("generate");
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+// Generate event listen
+generatetEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEL.innerText = generatePassword(
+    hasLower,
+    hasNumber,
+    hasUpper,
+    hasSymbol,
+    length
+    );
+});
+
+//Generate password function
+
+function generatePassword( lower, upper, number, symbol, length) {
+  // Init passwrd var
+  // filter out unchecked types
+  // loop over length call generator function for each type
+  // Add final passwrd to the passwrd variable and return
+
+  let generatedPassword = "";
+
+  const typesCount = lower + upper + number + symbol;
+  
+  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
+  (item => Object.values (item) [0]);
+
+  if (typesCount === 0) {
+    return "";
+  }
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type) [0];
+      generatedPassword += randomFunc[funcName] ();
+    });
+  }
+  const finalPassword = (generatedPassword)
+  return finalPassword
+  }
+
+
+//Generator Funtions
+
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26)+ 97);
+}
+
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26)+ 65);
+}
+
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10)+ 48);
+}
+
+function getRandomSymbol(){
+  const symbols = "!@#$%&^="
+  return symbols [Math.floor(Math.random() * symbols.length)];
+}
